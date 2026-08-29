9e3505fbf63c06f9bffc0e4751c3c436e80a5280
Z User <z@container>
2026-08-29 10:20:06 +0000

    v2.8.0

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === ВЕРСИЯ ПРИЛОЖЕНИЯ (менять только здесь!) ===
-const APP_VERSION = '2.7.4';
+const APP_VERSION = '2.8.0';
 const CACHE_NAME = `saddharma-app-v${APP_VERSION}`;
 
 const ASSETS = [
